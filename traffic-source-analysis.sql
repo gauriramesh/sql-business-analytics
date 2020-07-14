a624a90271bfac9c3d74b8214aa00428431489d3
gauri <gauri.ramesh15@gmail.com>
2020-07-14 10:59:47 -0500

    formatting

--- traffic-source-analysis.sql
+++ traffic-source-analysis.sql
@@ -1,15 +1,12 @@
-use mavenfuzzyfactory;
+USE mavenfuzzyfactory; 
 
-SELECT
-	website_sessions.utm_source,
-    website_sessions.utm_campaign,
-    http_referer,
-	COUNT(DISTINCT website_sessions.website_session_id) AS sessions
-FROM website_sessions
-WHERE website_sessions.created_at < "2012-04-12"
-GROUP BY
-	website_sessions.utm_source,
-    website_sessions.utm_campaign,
-    website_sessions.http_referer
-ORDER BY sessions DESC;
-
+SELECT website_sessions.utm_source, 
+       website_sessions.utm_campaign, 
+       http_referer, 
+       Count(DISTINCT website_sessions.website_session_id) AS sessions 
+FROM   website_sessions 
+WHERE  website_sessions.created_at < "2012-04-12" 
+GROUP  BY website_sessions.utm_source, 
+          website_sessions.utm_campaign, 
+          website_sessions.http_referer 
+ORDER  BY sessions DESC; 
